b164099d64200d35b4bf39f812ac5be12f052a87
Ben Rog-Wilhelm <zorba-githubdesktop20180420@pavlovian.net>
2018-09-03 15:30:40 -0700

    Add warning handler and some extra documentation.

--- test/Base.cs
+++ test/Base.cs
@@ -12,24 +12,59 @@ namespace DefTest
             Def.Database.Clear();
         }
 
+        private bool handlingWarnings = false;
+        private bool handledWarning = false;
+
         private bool handlingErrors = false;
         private bool handledError = false;
+
         [OneTimeSetUp]
         public void PrepHooks()
         {
+            Def.Config.WarningHandler = str => {
+                System.Diagnostics.Debug.Print(str);
+                Console.WriteLine(str);
+
+                if (handlingWarnings)
+                {
+                    handledWarning = true;
+                }
+                else
+                {
+                    // Throw if we're not handling it - this way we get test failures
+                    throw new ArgumentException(str);
+                }
+            };
+
             Def.Config.ErrorHandler = str => {
                 System.Diagnostics.Debug.Print(str);
                 Console.WriteLine(str);
 
                 if (handlingErrors)
                 {
+                    // If we're handling it, don't throw - this way we can validate that fallback behavior is working right
                     handledError = true;
                 }
                 else
                 {
+                    // Throw if we're not handling it - this way we get test failures and can validate that exception-passing behavior is working right
                     throw new ArgumentException(str);
                 }
             };
+        }
+
+        protected void ExpectWarnings(Action action)
+        {
+            Assert.IsFalse(handlingWarnings);
+            handlingWarnings = true;
+            handledWarning = false;
+
+            action();
+
+            Assert.IsTrue(handlingWarnings);
+            Assert.IsTrue(handledWarning);
+            handlingWarnings = false;
+            handledWarning = false;
         }
 
         protected void ExpectErrors(Action action)
