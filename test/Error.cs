db976b636ef1e30677dd8c13e924ac29d1a4d31f
Ben Rog-Wilhelm <zorba-githubdesktop20180420@pavlovian.net>
2018-09-03 15:31:45 -0700

    Add validation tests for root tag behavior.

--- test/Error.cs
+++ test/Error.cs
@@ -7,6 +7,18 @@ namespace DefTest
     public class Error : Base
     {
         [Test]
+        public void WarningTest()
+        {
+            // This doesn't happen normally, but does in our test framework
+            Assert.Throws(typeof(ArgumentException), () => Def.Config.WarningHandler("Test"));
+
+            ExpectWarnings(() => Def.Config.WarningHandler("Test"));
+
+            // Make sure things are deinited properly
+            Assert.Throws(typeof(ArgumentException), () => Def.Config.WarningHandler("Test"));
+        }
+
+        [Test]
         public void ErrorTest()
         {
             Assert.Throws(typeof(ArgumentException), () => Def.Config.ErrorHandler("Test"));
@@ -16,5 +28,42 @@ namespace DefTest
             // Make sure things are deinited properly
             Assert.Throws(typeof(ArgumentException), () => Def.Config.ErrorHandler("Test"));
         }
+
+        [Test]
+	    public void IncorrectRootTest()
+	    {
+            var parser = new Def.Parser();
+            ExpectWarnings(() => parser.ParseFromString(
+                @"
+                <NotDefs>
+                    <StubDef defName=""TestDef"">
+                        
+                    </StubDef>
+                </NotDefs>
+                ", new Type[]{ typeof(StubDef) }));
+
+            Assert.IsNotNull(Def.Database<StubDef>.Get("TestDef"));
+	    }
+
+        [Test]
+	    public void MultipleRootTest()
+	    {
+            var parser = new Def.Parser();
+            ExpectErrors(() => parser.ParseFromString(
+                @"
+                <Defs>
+                    <StubDef defName=""TestDefA"">
+                        
+                    </StubDef>
+                </Defs>
+                <Defs>
+                    <StubDef defName=""TestDefB"">
+                        
+                    </StubDef>
+                </Defs>
+                ", new Type[]{ typeof(StubDef) }));
+
+            // Currently not providing any guarantees on whether these get parsed; I'd actually like for them to get parsed, but doing so is tricky
+	    }
     }
 }
