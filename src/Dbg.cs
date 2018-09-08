144970e5ff2fbe0669446ba792568c4456ae29ff
Ben Rog-Wilhelm <zorba-githubdesktop20180420@pavlovian.net>
2018-09-08 02:13:28 -0700

    Ensure that inline formatted lines don't accidentally cause format errors.

--- src/Dbg.cs
+++ src/Dbg.cs
@@ -4,14 +4,29 @@ namespace Def
 
     internal static class Dbg
     {
+        internal static void Inf(string format)
+        {
+            Config.InfoHandler(format);
+        }
+
         internal static void Inf(string format, params object[] args)
         {
             Config.InfoHandler(string.Format(format, args));
         }
 
+        internal static void Wrn(string format)
+        {
+            Config.WarningHandler(format);
+        }
+
         internal static void Wrn(string format, params object[] args)
         {
             Config.WarningHandler(string.Format(format, args));
+        }
+
+        internal static void Err(string format)
+        {
+            Config.ErrorHandler(format);
         }
 
         internal static void Err(string format, params object[] args)
