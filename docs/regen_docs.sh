62d02760b2ada33cc8a530937ab7791e7f177e3e
Ben Rog-Wilhelm <zorba-githubdesktop20180420@pavlovian.net>
2018-10-14 08:34:19 -0700

    Update docs script.

--- docs/regen_docs.sh
+++ docs/regen_docs.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# obliterate caches, just for safety's sake
+rm -rf docs/obj docs/bin docs/packages docs/DROP docs/TEMP
+
+# get the repo, make sure there's no files
 rm -rf docs/_site
 git clone -b gh-pages . docs/_site
 rm -rf docs/_site/*
 
-docfx `cygpath -a -w docs/docfx.json`
+# generate docs
+docfx docs/docfx.json
 
+# update repo
 (cd docs/_site && git add . && git commit -a -m "Update documentation." && git push)
 
+# clear repo directory
 rm -rf docs/_site
