10a1f7eea5e4c94088ca5a859aa9b8d9543550e3
Ben Rog-Wilhelm <zorba-githubdesktop20180420@pavlovian.net>
2020-05-03 08:52:49 -0500

    LOAF: Split armor into its own section in the inventory list.

--- example/loaf/Locations.cs
+++ example/loaf/Locations.cs
@@ -16,9 +16,10 @@ namespace Loaf.Locations
             Cns.Out("Your health is restored to full!");
             Cns.Out("It was full anyway because this game doesn't store health between battles.");
             Cns.Out("You take the opportunity to rummage through your pockets.");
+
             Cns.Out("");
             Cns.Out("You are carrying:");
-            foreach (var item in Player.Instance.Inventory)
+            foreach (var item in Player.Instance.Inventory.Where(item => !(item is ArmorDef)))
             {
                 Cns.Out($"  {item.name}");
             }
@@ -26,6 +27,17 @@ namespace Loaf.Locations
             {
                 Cns.Out($"  {Player.Instance.Gold} gold");
             }
+
+            if (Player.Instance.Inventory.Any(item => item is ArmorDef))
+            {
+                Cns.Out("");
+                Cns.Out("You are wearing:");
+                foreach (var item in Player.Instance.Inventory.Where(item => item is ArmorDef))
+                {
+                    Cns.Out($"  {item.name}");
+                }
+            }
+
             Cns.Out("");
             Cns.Out($"You are currently wielding a {Player.Instance.CurrentWeapon.name}.");
             Cns.Out("");
