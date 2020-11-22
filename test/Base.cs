368aebed7228eeb3e90e7501a15a2ef935c42634
Ben Rog-Wilhelm <zorba-githubdesktop20180420@pavlovian.net>
2020-11-22 11:44:55 -0600

    Move code generation into a shared function.

--- test/Base.cs
+++ test/Base.cs
@@ -129,6 +129,52 @@ namespace DefTest
         {
         }
 
+        private void RunCode(string code, string functionName, params object[] param)
+        {
+            string source = @"
+                    using NUnit.Framework;
+
+                    public static class TestClass
+                    {
+                        " + code + @"
+                    }";
+
+            var syntaxTree = CSharpSyntaxTree.ParseText(source);
+            string assemblyName = Path.GetRandomFileName();
+            var refPaths = new[] {
+                    typeof(object).GetTypeInfo().Assembly.Location,
+                    typeof(Def.Def).GetTypeInfo().Assembly.Location,
+                    typeof(NUnit.Framework.Assert).GetTypeInfo().Assembly.Location,
+                    this.GetType().GetTypeInfo().Assembly.Location,
+                    Path.Combine(Path.GetDirectoryName(typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly.Location), "netstandard.dll"),
+                    Path.Combine(Path.GetDirectoryName(typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly.Location), "System.Runtime.dll"),
+                    Path.Combine(Path.GetDirectoryName(typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly.Location), "System.Collections.dll"),
+                };
+
+            var references = refPaths.Select(r => MetadataReference.CreateFromFile(r)).ToArray();
+
+            var compilation = CSharpCompilation.Create(
+                assemblyName,
+                syntaxTrees: new[] { syntaxTree },
+                references: references,
+                options: new CSharpCompilationOptions(Microsoft.CodeAnalysis.OutputKind.DynamicallyLinkedLibrary));
+
+            var ms = new MemoryStream();
+            var result = compilation.Emit(ms);
+            if (!result.Success)
+            {
+                Assert.IsTrue(false, string.Join("\n", result.Diagnostics.Select(err => err.ToString())));
+            }
+
+            ms.Seek(0, SeekOrigin.Begin);
+
+            var assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
+            var t = assembly.GetType("TestClass");
+            var m = t.GetMethod(functionName);
+
+            m.Invoke(null, param);
+        }
+
         // Everything after here is designed for the Behavior tests, where we run tests in a variety of ways to test serialization.
 
         public enum BehaviorMode
@@ -190,11 +236,11 @@ namespace DefTest
             }
             else if (mode == BehaviorMode.Validation)
             {
-                string data = null;
+                string code = null;
                 void RunComposer()
                 {
                     var composer = new Def.Composer();
-                    data = composer.ComposeValidation();
+                    code = composer.ComposeValidation();
                 }
 
                 if (validation_expectWriteErrors)
@@ -206,51 +252,7 @@ namespace DefTest
                     RunComposer();
                 }
 
-                string source = @"
-                    using NUnit.Framework;
-
-                    public static class TestClass
-                    { 
-                        public static void Eval()
-                        {" +
-                            data + @"
-                        } 
-                    }";
-
-                var syntaxTree = CSharpSyntaxTree.ParseText(source);
-                string assemblyName = Path.GetRandomFileName();
-                var refPaths = new[] {
-                    typeof(object).GetTypeInfo().Assembly.Location,
-                    typeof(Def.Def).GetTypeInfo().Assembly.Location,
-                    typeof(NUnit.Framework.Assert).GetTypeInfo().Assembly.Location,
-                    this.GetType().GetTypeInfo().Assembly.Location,
-                    Path.Combine(Path.GetDirectoryName(typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly.Location), "netstandard.dll"),
-                    Path.Combine(Path.GetDirectoryName(typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly.Location), "System.Runtime.dll"),
-                    Path.Combine(Path.GetDirectoryName(typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly.Location), "System.Collections.dll"),
-                };
-
-                var references = refPaths.Select(r => MetadataReference.CreateFromFile(r)).ToArray();
-
-                var compilation = CSharpCompilation.Create(
-                    assemblyName,
-                    syntaxTrees: new[] { syntaxTree },
-                    references: references,
-                    options: new CSharpCompilationOptions(Microsoft.CodeAnalysis.OutputKind.DynamicallyLinkedLibrary));
-
-                var ms = new MemoryStream();
-                var result = compilation.Emit(ms);
-                if (!result.Success)
-                {
-                    Assert.IsTrue(false, string.Join("\n", result.Diagnostics.Select(err => err.ToString())));
-                }
-
-                ms.Seek(0, SeekOrigin.Begin);
-
-                var assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
-                var t = assembly.GetType("TestClass");
-                var m = t.GetMethod("Eval");
-
-                m.Invoke(null, null);
+                RunCode($"public static void Test() {{\n{code}\n}}", "Test", null);
             }
             else
             {
