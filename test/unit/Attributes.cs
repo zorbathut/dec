5a43f1921022b6e5e2cb58cdcb178c7c6750ec73
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2021-10-05 11:33:11 -0500

    Add a pile of behavior tests for attribute combinations (mostly invalid ones).

---
+++ test/unit/Attributes.cs
@@ -0,0 +1,132 @@
+namespace DecTest
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [TestFixture]
+    public class Attribute : Base
+    {
+        public class BaseString : Dec.IRecordable
+        {
+            public string value = "default";
+
+            public void Record(Dec.Recorder recorder)
+            {
+                recorder.Record(ref value, "value");
+            }
+        }
+
+        public class DerivedString : BaseString
+        {
+
+        }
+
+        public class StringMemberRecordable : Dec.IRecordable
+        {
+            public BaseString member;
+
+            public void Record(Dec.Recorder recorder)
+            {
+                recorder.Record(ref member, "member");
+            }
+        }
+
+        public class StringMemberDec : Dec.Dec
+        {
+            public BaseString member;
+        }
+
+        [Test]
+        public void Record([Values] bool classTag, [Values] bool nullTag, [Values] bool refTag)
+        {
+            Dec.Config.TestParameters = new Dec.Config.UnitTestParameters { explicitTypes = new Type[] { typeof(BaseString), typeof(DerivedString) } };
+
+            string serialized = $@"
+                <Record>
+                  <recordFormatVersion>1</recordFormatVersion>
+                  <refs>
+                    <Ref id=""ref00000"" class=""BaseString""><value>reffed</value></Ref>
+                  </refs>
+                  <data>
+                    <member {(classTag ? "class='DerivedString'" : "")} {(nullTag ? "null='true'" : "")} {(refTag ? "ref='ref00000'" : "")}><value>data</value></member>
+                  </data>
+                </Record>";
+
+            StringMemberRecordable deserialized = null;
+
+            int tags = (classTag ? 1 : 0) + (nullTag ? 1 : 0) + (refTag ? 1 : 0);
+
+            if (tags <= 1)
+            {
+                deserialized = Dec.Recorder.Read<StringMemberRecordable>(serialized);
+            }
+            else
+            {
+                ExpectErrors(() => deserialized = Dec.Recorder.Read<StringMemberRecordable>(serialized));
+            }
+
+            if (refTag)
+            {
+                Assert.IsInstanceOf<BaseString>(deserialized.member);
+                Assert.AreEqual("reffed", deserialized.member.value);
+            }
+            else if (nullTag)
+            {
+                Assert.IsNull(deserialized.member);
+            }
+            else if (classTag)
+            {
+                Assert.IsInstanceOf<DerivedString>(deserialized.member);
+                Assert.AreEqual("data", deserialized.member.value);
+            }
+            else
+            {
+                Assert.IsInstanceOf<BaseString>(deserialized.member);
+                Assert.AreEqual("data", deserialized.member.value);
+            }
+        }
+
+        [Test]
+        public void Parser([Values] BehaviorMode mode, [Values] bool classTag, [Values] bool nullTag, [Values] bool refTag)
+        {
+            Dec.Config.TestParameters = new Dec.Config.UnitTestParameters { explicitTypes = new Type[] { typeof(StringMemberDec), typeof(DerivedString) } };
+
+            var parser = new Dec.Parser();
+            parser.AddString($@"
+                <Decs>
+                    <StringMemberDec decName=""TestDec"">
+                        <member {(classTag ? "class='DerivedString'" : "")} {(nullTag ? "null='true'" : "")} {(refTag ? "ref='ref00000'" : "")}><value>data</value></member>
+                    </StringMemberDec>
+                </Decs>");
+
+            int tags = ( classTag ? 1 : 0 ) + ( nullTag ? 1 : 0 ) + ( refTag ? 1 : 0 );
+            if (tags <= 1 && !refTag)
+            {
+                parser.Finish();
+            }
+            else
+            {
+                ExpectErrors(() => parser.Finish());
+            }
+
+            DoBehavior(mode);
+
+            if (nullTag)
+            {
+                Assert.IsNull(Dec.Database<StringMemberDec>.Get("TestDec").member);
+            }
+            else if (classTag)
+            {
+                Assert.IsInstanceOf<DerivedString>(Dec.Database<StringMemberDec>.Get("TestDec").member);
+                Assert.AreEqual("data", Dec.Database<StringMemberDec>.Get("TestDec").member.value);
+            }
+            else
+            {
+                Assert.IsInstanceOf<BaseString>(Dec.Database<StringMemberDec>.Get("TestDec").member);
+                Assert.AreEqual("data", Dec.Database<StringMemberDec>.Get("TestDec").member.value);
+            }
+        }
+    }
+}
