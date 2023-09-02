2978910e4b4cfcf22c6af527c011877599896940
Ben Rog-Wilhelm <zorba-git-sharon-vfio@pavlovian.net>
2023-09-01 22:55:01 -0500

    Test: Bad list tags.

--- test/unit/CollectionList.cs
+++ test/unit/CollectionList.cs
@@ -73,5 +73,45 @@ namespace DecTest
             Assert.AreEqual(result.dataB, new int[0] );
             Assert.AreEqual(result.dataC, new[] { 9, 10, 11 });
         }
+
+        [Test]
+        public void BadTags([Values] ParserMode mode)
+        {
+            UpdateTestParameters(new Dec.Config.UnitTestParameters { explicitTypes = new Type[] { typeof(ListDec) } });
+
+            var parser = new Dec.Parser();
+            parser.AddString(Dec.Parser.FileType.Xml, @"
+                <Decs>
+                    <ListDec decName=""TestDecA"">
+                        <data>
+                            <horse>10</horse>
+                            <li>9</li>
+                            <li>8</li>
+                            <li>7</li>
+                            <li>6</li>
+                        </data>
+                    </ListDec>
+                    <ListDec decName=""TestDecB"">
+                        <data>
+                            <li>10</li>
+                            <li>9</li>
+                            <rhino>8</rhino>
+                            <li>7</li>
+                            <li>6</li>
+                        </data>
+                    </ListDec>
+                </Decs>");
+            ExpectErrors(() => parser.Finish());
+
+            DoParserTests(mode);
+
+            var tda = Dec.Database<ListDec>.Get("TestDecA");
+            Assert.IsNotNull(tda);
+            Assert.AreEqual(tda.data, new[] { 10, 9, 8, 7, 6 });
+
+            var tdb = Dec.Database<ListDec>.Get("TestDecB");
+            Assert.IsNotNull(tdb);
+            Assert.AreEqual(tdb.data, new[] { 10, 9, 8, 7, 6 });
+        }
     }
 }
