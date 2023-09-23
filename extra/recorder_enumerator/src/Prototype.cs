b1a6de32e9e8e2290d1a65226233fca618d74196
Ben Rog-Wilhelm <zorba-git-sharon-vfio@pavlovian.net>
2023-09-23 16:31:29 -0500

    First-time Parser startup speed now faster.

---
+++ extra/recorder_enumerator/src/Prototype.cs
@@ -0,0 +1,7 @@
+namespace Dec.RecorderCoroutine
+{
+    public static class Prototype
+    {
+        public static int ReturnTwo() { return 2; }
+    }
+}
---
+++ extra/recorder_enumerator/test/Linq.cs
@@ -0,0 +1,16 @@
+namespace RecorderCoroutineTest
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+
+    [TestFixture]
+    public class Primitives : DecTest.Base
+    {
+        [Test]
+        public void Stub()
+        {
+            
+        }
+    }
+}
