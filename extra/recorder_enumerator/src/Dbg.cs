c0b03c7af6b08d3154875fb27bdc2583b2e4ce81
Ben Rog-Wilhelm <zorba-git-sharon-vfio@pavlovian.net>
2023-09-26 00:23:54 -0500

    Recorder_Enumerator: Decorate RecorderEnumerator debug output.

--- extra/recorder_enumerator/src/Dbg.cs
+++ extra/recorder_enumerator/src/Dbg.cs
@@ -6,17 +6,17 @@ namespace Dec.RecorderEnumerator
     {
         internal static void Inf(string str)
         {
-            global::Dec.Config.InfoHandler(str);  
+            global::Dec.Config.InfoHandler("RecorderEnumerator: " + str);  
         }
 
         internal static void Wrn(string str)
         {
-            global::Dec.Config.WarningHandler(str);
+            global::Dec.Config.WarningHandler("RecorderEnumerator: " + str);
         }
 
         internal static void Err(string str)
         {
-            global::Dec.Config.ErrorHandler(str);
+            global::Dec.Config.ErrorHandler("RecorderEnumerator: " + str);
         }
 
         internal static void Ex(Exception e)
