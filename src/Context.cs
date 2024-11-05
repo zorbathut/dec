b28e6116939cef856cc1140df603ea6112bdb172
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2024-11-04 18:41:03 -0600

    Move Context into its own file.

---
+++ src/Context.cs
@@ -0,0 +1,45 @@
+namespace Dec
+{
+    /// <summary>
+    /// Information on the current cursor position when reading files.
+    /// </summary>
+    /// <remarks>
+    /// Standard output format is $"{inputContext}: Your Error Text Here!". This abstracts out the requirements for generating the locational-context text.
+    ///
+    /// This is a struct to cut down on GC churn.
+    /// </remarks>
+    public struct Context
+    {
+        internal string filename;
+        internal System.Xml.Linq.XElement element;
+        internal Path path;
+
+        internal Context(string filename = null, System.Xml.Linq.XElement element = null, Path path = null)
+        {
+            this.filename = filename;
+            this.element = element;
+            this.path = path;
+        }
+
+        public override string ToString()
+        {
+            if (this.element != null)
+            {
+                return $"{filename}:{element.LineNumber()}";
+            }
+            else if (filename != null)
+            {
+                return filename;
+            }
+            else if (path != null)
+            {
+                return path.ToString();
+            }
+            else
+            {
+                // shrug
+                return "[unknown]";
+            }
+        }
+    }
+}
