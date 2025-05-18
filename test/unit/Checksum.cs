218f4b075b4b944f66cf699f89c9014a37549249
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2025-05-18 18:38:32 -0500

    Test: Whole big batch of dedicated Checksum tests.

---
+++ test/unit/Checksum.cs
@@ -0,0 +1,332 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace DecTest
+{
+    [TestFixture]
+    public class Checksum : Base
+    {
+        [Test]
+        public void Integer()
+        {
+            int value1 = 5;
+            int value2 = 10;
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different integers should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical integers should produce the same checksums");
+        }
+
+        [Test]
+        public void Long()
+        {
+            long value1 = 9876543210;
+            long value2 = -9876543210;
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different longs should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical longs should produce the same checksums");
+        }
+
+        [Test]
+        public void ULong()
+        {
+            ulong value1 = 18446744073709551615UL; // max ulong
+            ulong value2 = 9223372036854775808UL;  // half of max
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different ulongs should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical ulongs should produce the same checksums");
+        }
+
+        [Test]
+        public void UInt()
+        {
+            uint value1 = 4294967295;  // max uint
+            uint value2 = 2147483648;  // half of max
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different uints should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical uints should produce the same checksums");
+        }
+
+        [Test]
+        public void Short()
+        {
+            short value1 = 32767;  // max short
+            short value2 = -32768; // min short
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different shorts should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical shorts should produce the same checksums");
+        }
+
+        [Test]
+        public void UShort()
+        {
+            ushort value1 = 65535;  // max ushort
+            ushort value2 = 32768;  // half of max
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different ushorts should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical ushorts should produce the same checksums");
+        }
+
+        [Test]
+        public void SByte()
+        {
+            sbyte value1 = 127;  // max sbyte
+            sbyte value2 = -128; // min sbyte
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different sbytes should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical sbytes should produce the same checksums");
+        }
+
+        [Test]
+        public void Byte()
+        {
+            byte value1 = 255; // max byte
+            byte value2 = 128; // half of max
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different bytes should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical bytes should produce the same checksums");
+        }
+
+        [Test]
+        public void Float()
+        {
+            float value1 = 3.14159f;
+            float value2 = 2.71828f;
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different floats should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical floats should produce the same checksums");
+        }
+
+        [Test]
+        public void Double()
+        {
+            double value1 = 3.14159265358979;
+            double value2 = 2.71828182845905;
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different doubles should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical doubles should produce the same checksums");
+        }
+
+        [Test]
+        public void Boolean()
+        {
+            bool value1 = true;
+            bool value2 = false;
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different booleans should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical booleans should produce the same checksums");
+        }
+
+        [Test]
+        public void String()
+        {
+            string value1 = "Hello, world!";
+            string value2 = "Hello, World!"; // Capital W
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different strings should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical strings should produce the same checksums");
+        }
+
+        [Test]
+        public void Enum()
+        {
+            GenericEnum value1 = GenericEnum.Alpha;
+            GenericEnum value2 = GenericEnum.Beta;
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different enums should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical enums should produce the same checksums");
+        }
+
+        [Test]
+        public void Type()
+        {
+            Type value1 = typeof(int);
+            Type value2 = typeof(string);
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different types should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical types should produce the same checksums");
+        }
+
+        [Test]
+        public void DecObject()
+        {
+            UpdateTestParameters(new Dec.Config.UnitTestParameters { explicitTypes = new Type[] { typeof(StubDec) } });
+
+            var parser = new Dec.Parser();
+            parser.AddString(Dec.Parser.FileType.Xml, @"
+                <Decs>
+                    <StubDec decName=""TestDecA"" />
+                    <StubDec decName=""TestDecB"" />
+                </Decs>");
+            parser.Finish();
+
+            StubDec value1 = Dec.Database<StubDec>.Get("TestDecA");
+            StubDec value2 = Dec.Database<StubDec>.Get("TestDecB");
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different decs should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical decs should produce the same checksums");
+        }
+
+        [Test]
+        public void Array()
+        {
+            int[] value1 = new int[] { 1, 2, 3 };
+            int[] value2 = new int[] { 1, 2, 4 };
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different arrays should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical arrays should produce the same checksums");
+        }
+
+        [Test]
+        public void List()
+        {
+            var value1 = new List<int> { 1, 2, 3 };
+            var value2 = new List<int> { 1, 2, 4 };
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different lists should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical lists should produce the same checksums");
+        }
+
+        [Test]
+        public void Dictionary()
+        {
+            var value1 = new Dictionary<string, int> { { "a", 1 }, { "b", 2 } };
+            var value2 = new Dictionary<string, int> { { "a", 1 }, { "b", 3 } };
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different dictionaries should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical dictionaries should produce the same checksums");
+        }
+
+        [Test]
+        public void HashSet()
+        {
+            var value1 = new HashSet<int> { 1, 2, 3 };
+            var value2 = new HashSet<int> { 1, 2, 4 };
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different hashsets should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical hashsets should produce the same checksums");
+        }
+
+        [Test]
+        public void Queue()
+        {
+            var value1 = new Queue<int>(new[] { 1, 2, 3 });
+            var value2 = new Queue<int>(new[] { 1, 2, 4 });
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different queues should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical queues should produce the same checksums");
+        }
+
+        [Test]
+        public void Stack()
+        {
+            var value1 = new Stack<int>(new[] { 1, 2, 3 });
+            var value2 = new Stack<int>(new[] { 1, 2, 4 });
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different stacks should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical stacks should produce the same checksums");
+        }
+
+        [Test]
+        public void Tuple()
+        {
+            var value1 = System.Tuple.Create(1, "hello");
+            var value2 = System.Tuple.Create(1, "world");
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different tuples should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical tuples should produce the same checksums");
+        }
+
+        [Test]
+        public void ValueTuple()
+        {
+            var value1 = (1, "hello");
+            var value2 = (1, "world");
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreNotEqual(checksum1, checksum2, "Different value tuples should produce different checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical value tuples should produce the same checksums");
+        }
+
+        [Test]
+        public void IRecordable()
+        {
+            var value1 = new StubRecordable();
+            var value2 = new StubRecordable();
+
+            ulong checksum1 = Dec.Recorder.Checksum(value1);
+            ulong checksum2 = Dec.Recorder.Checksum(value2);
+
+            Assert.AreEqual(checksum1, checksum2, "Empty recordables should produce the same checksums");
+            Assert.AreEqual(checksum1, Dec.Recorder.Checksum(value1), "Identical recordables should produce the same checksums");
+        }
+    }
+}
