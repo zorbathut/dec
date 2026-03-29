278f9bcf5082bd0269779c91f37ac457cb996791
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2026-03-29 07:23:52 -0500

    Fix: Benchmark autorun.

--- extra/benchmark/Program.cs
+++ extra/benchmark/Program.cs
@@ -1,7 +1,6 @@
 using BenchmarkDotNet.Running;
 
 var switcher = BenchmarkSwitcher.FromAssembly(typeof(DecBenchmark.BenchmarkBase).Assembly);
-if (args.Length == 0)
-    switcher.RunAll();
-else
-    switcher.Run(args);
+if (!args.Any(a => a == "--filter"))
+    args = args.Prepend("--filter").Append("*").ToArray();
+switcher.Run(args);
